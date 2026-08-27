308f34175b99805e605846d9791745d6c874595b
mohiuddinamaman-sudo <mohiuddin.am.aman@gmail.com>
2026-08-27 18:54:22 +0530

    Update fraud_analysis.sql

--- fraud_analysis.sql
+++ fraud_analysis.sql
@@ -8,3 +8,35 @@ FROM transactions t
 LEFT JOIN users u
     ON t.USER_ID = u.ID
 WHERE t.SOURCE = 'GAIA';
+
+
+
+-- A.2: Transaction Success Rate
+
+WITH first_card_payment AS (
+    SELECT
+        t.USER_ID,
+        t.CREATED_DATE,
+        t.AMOUNT,
+        t.CURRENCY,
+        ROW_NUMBER() OVER (
+            PARTITION BY t.USER_ID
+            ORDER BY t.CREATED_DATE
+        ) AS rn
+    FROM transactions t
+    WHERE t.TYPE = 'CARD_PAYMENT'
+      AND t.STATE = 'COMPLETED'
+)
+
+SELECT
+    COUNT(*) AS successful_users,
+    (SELECT COUNT(*) FROM users) AS total_users,
+    COUNT(*) * 100.0 / (SELECT COUNT(*) FROM users) AS success_rate
+FROM first_card_payment f
+JOIN currency_details c
+    ON f.CURRENCY = c.CURRENCY
+JOIN fx_rates r
+    ON r.BASE_CCY = 'EUR'
+   AND r.CCY = 'USD'
+WHERE f.rn = 1
+  AND (f.AMOUNT / POWER(10, c.EXPONENT)) * r.RATE > 10;
